ae20f85de8a85fd6ed86b6f2735d3625728b1ae8
thangciu <53205940+thangciu@users.noreply.github.com>
2020-02-02 14:59:17 +0700

    init project

---
+++ react-src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "index.scss";
+
+
+ReactDOM.render( <div>
+    <div> Hello </div> </div > ,
+
+    document.getElementById("root"));
